72f11c29e7e16ee09f7bbb259e06d07e466a3416
Sergio Bernal <sergiobernal909@gmail.com>
2021-01-24 04:57:22 +0000

    Styling layout and tile

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,27 +6,39 @@ import InfoTileList from '../../components/InfoTileList';
 import Loader from '../../components/Loader';
 
 const Home = () => {
-
-  const { 
-      result, 
-      loading 
+  const {
+    result,
+    loading
   } = useFetch(`${process.env.REACT_APP_API_URL}/transactions?project=6008ff93be76f7423086c5f5`);
-  if (loading) 
+  console.log(result);
+  if (loading)
     return <Loader />;
-  const transactionsData  = result.result;
-  const { gastos, countGastos, ingresos, countIngresos } = transactionsData;
+  const transactionsData = result.result;
+  const {
+    gastos,
+    countGastos, 
+    ingresos, 
+    countIngresos,
+    saldo,
+    countTransacciones 
+    } = transactionsData;
   return (
     <Container>
       <InfoTileList>
-        <InfoTile 
-          numberTransactions={countIngresos} 
-          amount={ingresos} 
-          transactionType='Ingresos' 
+        <InfoTile
+          numberTransactions={countIngresos}
+          amount={ingresos}
+          transactionType='Ingresos'
         />
-        <InfoTile 
-          numberTransactions={countGastos} 
-          amount={gastos} 
-          transactionType='Gastos' 
+        <InfoTile
+          numberTransactions={countGastos}
+          amount={gastos}
+          transactionType='Gastos'
+        />
+        <InfoTile
+          numberTransactions={countTransacciones}
+          amount={saldo}
+          transactionType='Saldo'
         />
       </InfoTileList>
     </Container>
